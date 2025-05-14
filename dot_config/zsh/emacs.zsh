aab16a795e1b01bb5c1dd19fddd87749cb28e16a
Mason Katz <Mason.Katz@Teradata.com>
2025-05-14 15:37:02 -0700

    fix(zsh): make emacsclient the default editor

--- dot_config/zsh/emacs.zsh
+++ dot_config/zsh/emacs.zsh
@@ -1,9 +1,10 @@
 #!/bin/zsh
 
-export VISUAL=e
-export EDITOR=e
+export VISUAL=emacsclient
+export EDITOR=emacsclient
+export ALTERNATE_EDITOR="emacs -nw"	# emacs client uses this
 
-alias e='emacsclient --alternate-editor=" " -c'
+alias e="emacsclient -c"
 
 function restart-emacs() {
 	if emacsclient -e '(progn (message "Killing Emacs...") (kill-emacs))' >/dev/null 2>&1; then
